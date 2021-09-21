b0ecfb973ff6223752180014a43d5b4e7502583a
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-09-21 18:38:55 +0200

    add cones

--- src/SymbolicCRN.jl
+++ src/SymbolicCRN.jl
@@ -12,4 +12,11 @@ export filter_isreal, realpositiveroots, collect_realpositiveroots, pRoots_qPoss
 # using PolynomialRoots: roots
 include("FindRoots.jl")
 
+
+export cone_positiveorthant, cone_vectorspace, cone_positivenullspace,
+findnegativepoint, rays_outernormalcone, outernormalcone
+## Nemo: matrix, FlintZZ, nullspace
+## Polymake: polytope:cone,intersection
+include("Cones.jl")
+
 end
