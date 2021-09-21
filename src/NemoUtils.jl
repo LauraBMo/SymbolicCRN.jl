ce593be79441f737716c3dd7c7a6c90bcf25293b
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-09-21 20:02:47 +0200

    add NemoUtils

---
+++ src/NemoUtils.jl
@@ -0,0 +1,88 @@
+
+
+# const AA = AbstractAlgebra
+
+@doc raw"""
+
+    dissect(p::MPolyElem)
+
+Iterator for the coefficients and exponent vectors of the given polynomial.
+
+"""
+function dissect(p::MPolyElem)
+    return zip(coeffs(p), exponent_vectors(p))
+end
+
+get_coeffs(p, I=0:length(p)) = [coeff(p, i) for i in I]
+
+normalize(p) = divexact(p, content(p))
+
+lead(i::Number) = i
+
+function Nemo.nullspace_right_rational(N::AbstractArray{T}) where {T <: Integer}
+    Nnemo = Nemo.matrix(Nemo.FlintZZ, N)
+    r, U = Nemo.nullspace_right_rational(Nnemo)
+    return convert_to_array(U[:,1:r], T)
+end
+
+"""
+
+    matrix(R, V::AbstractVector{T}) where {T}
+
+Given a Julia vector `V` of entries, construct the corresponding AbstractAlgebra.jl one-column matrix over the given ring `R`, assuming all the entries can be coerced into `R`.
+"""
+function Nemo.matrix(R::AbstractAlgebra.Ring, V::AbstractVector)
+    return Nemo.matrix(R, reshape(V, (:, 1)))
+end
+
+
+@doc raw"""
+
+    Diagonal(R, V::AbstractVector{T}) where {T}
+
+Given a Julia vector `V` of entries, construct the corresponding AbstractAlgebra.jl diagonal matrix over the given ring `R`, assuming all the entries can be coerced into `R`.
+"""
+function Diagonal(R::AbstractAlgebra.Ring, V::AbstractVector{T}) where {T}
+    n = size(V, 1)
+    D = Nemo.zero_matrix(R, n, n)
+    for i in 1:n D[i,i] = V[i] end
+    return D
+end
+
+partialdervativeof(i::Integer) = p -> Nemo.derivative(p, i)
+
+"""
+
+    Jacobian(R, M)
+    Jacobian(R, M, vars=1:length(gens(R)))
+
+Returns the Jacobian matrix of a one-column matrix of polynomials `M` with respect to the generators of `R` indexed by `vars`. When `vars` is omitted all the generators of `R` are used.
+
+# Examples
+```jldoctest; setup = :(using CRNT, Nemo)
+julia> using Nemo
+
+julia> R, vars = PolynomialRing(ZZ, vcat(["k\$i" for i in 1:5], ["x\$i" for i in 1:4]));
+
+julia> M = [vars[1]*vars[2]*vars[6]-vars[8]; vars[3]*vars[9]+2*vars[7]]
+[k1*k2*x1-x3]
+[ k3*x4+2*x2]
+
+julia> Jacobian(R, M, 6:9)
+[k1*k2  0  -1   0]
+[    0  2   0  k3]
+
+julia> Jacobian(R,M)
+[k2*x1  k1*x1   0  0  0  k1*k2  0  -1   0]
+[    0      0  x4  0  0      0  2   0  k3]
+```
+"""
+function Jacobian(R::MPolyRing, M::AbstractVector, vars=1:length(gens(R)))
+    rows = size(M, 1)
+    cols = length(vars)
+    J = fill(zero(R), rows, cols)
+    for j in 1:cols
+        J[:,j] = partialdervativeof(vars[j]).(M)
+    end
+    return J
+end
--- src/SymbolicCRN.jl
+++ src/SymbolicCRN.jl
@@ -7,6 +7,10 @@ import LinearAlgebra: dot, transpose
 import PolynomialRoots
 
 
+export dissect, Diagonal, partialdervativeof, Jacobian
+include("NemoUtils.jl")
+
+
 export filter_isreal, realpositiveroots, collect_realpositiveroots, pRoots_qPossitive
 # using LinearAlgebra: dot
 # using PolynomialRoots: roots
@@ -14,7 +18,7 @@ include("FindRoots.jl")
 
 
 export cone_positiveorthant, cone_vectorspace, cone_positivenullspace,
-findnegativepoint, rays_outernormalcone, outernormalcone
+    findnegativepoint, rays_outernormalcone, outernormalcone
 ## Nemo: matrix, FlintZZ, nullspace
 ## Polymake: polytope:cone,intersection
 include("Cones.jl")
@@ -23,12 +27,12 @@ include("Cones.jl")
 export negvertices, posvertices
 include("PosNegVertices.jl")
 
+
 export evenodd_coeffs, evenodd_polys, mix_evenodd, principaldeterminantsof,
     normalize_Hurwitz, Hurwitzmatrix, Hurwitzmatrix_sylvester, Hurwitzdeterminants,
     Hurwitz_odd_determinants, Hurwitz_subresultants, Hurwitzdeterminants_subresultant,
     signs_stabilitymatrices
 include("Stability.jl")
-
 
 # using Catalyst
 # include("Catalyst.jl")
